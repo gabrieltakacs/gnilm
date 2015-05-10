8624adbb9ca0312f54850233a9af9df7e9464d41
Gabriel Takács <gtakacs17@gmail.com>
2015-05-11 01:26:07 +0200

    Add computations to a new thread

--- src/Controllers/Listeners/RunActionListener.java
+++ src/Controllers/Listeners/RunActionListener.java
@@ -15,32 +15,35 @@ public class RunActionListener extends ActionListenerAbstract {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        try {
-            House house = DataFactory.getHouse(Configuration.getInstance().getInputDirectory());
+        (new Thread() {
+            public void run() {
+                try {
 
-            Processor processor = new Processor(this.controller);
+                    House house = DataFactory.getHouse(Configuration.getInstance().getInputDirectory());
+                    Processor processor = new Processor(controller);
 
-            // TODO: toto treba presunut do processoru
-            processor.addTrainDataChannel(house.getChannel("kitchen1").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("lighting").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("stove").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("microwave").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("washer").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("kitchen2").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("refridgerator").setWindowThreshold(10.0));
-            processor.addTrainDataChannel(house.getChannel("dishwasher").setWindowThreshold(10.0).setCurrentValue(1205.67));
-            processor.addTrainDataChannel(house.getChannel("disposal").setWindowThreshold(10.0));
-            processor.detectEvents();
+                    // TODO: toto treba presunut do processoru
+                    processor.addTrainDataChannel(house.getChannel("kitchen1").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("lighting").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("stove").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("microwave").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("washer").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("kitchen2").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("refridgerator").setWindowThreshold(10.0));
+                    processor.addTrainDataChannel(house.getChannel("dishwasher").setWindowThreshold(10.0).setCurrentValue(1205.67));
+                    processor.addTrainDataChannel(house.getChannel("disposal").setWindowThreshold(10.0));
+                    processor.detectEvents();
 
-            ArrayList<Channel> applianceChannels = processor.getApplianceChannels();
-            Recommender recommender = new Recommender();
-            recommender.setController(this.controller);
-            for (Iterator<Channel> iterator = applianceChannels.iterator(); iterator.hasNext();) {
-                recommender.generateRecommendations(iterator.next());
+                    ArrayList<Channel> applianceChannels = processor.getApplianceChannels();
+                    Recommender recommender = new Recommender();
+                    recommender.setController(controller);
+                    for (Iterator<Channel> iterator = applianceChannels.iterator(); iterator.hasNext(); ) {
+                        recommender.generateRecommendations(iterator.next());
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
             }
-
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
+        }).start();
     }
 }
